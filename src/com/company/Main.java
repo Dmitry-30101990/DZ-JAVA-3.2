84c0b52ace39c09ebb4deeb4e74fbe5bd204de5e
Dmitry Zhmakin <dima8837922@gmail.com>
2021-04-18 20:07:29 +0700

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int transfer = 1300;
-	int count = 240;
-	if (transfer > 1000) {
-	int bonus = 100;
-	int amountbonuses = transfer / bonus;
-	System.out.print("Accrued bonuses "+amountbonuses+"");
-	}
+        int transfer = 1300;
+        int count = 240;
+        if (transfer > 1000) {
+            int bonus = 100;
+            int amountbonuses = transfer / bonus;
+            System.out.print("Accrued bonuses " + amountbonuses + "");
+        }
     }
