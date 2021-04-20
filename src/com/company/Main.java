a3b0b5e6749fdbc3edbc2715dd0f21906f9587fc
Dmitry Zhmakin <dima8837922@gmail.com>
2021-04-20 15:47:34 +0700

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,14 @@ public class Main {
     public static void main(String[] args) {
         int transfer = 1300;
         int count = 240;
+        int bonus;
         if (transfer > 1000) {
-            int bonus = 100;
-            int amountbonuses = transfer / bonus;
-            System.out.print("Accrued bonuses " + amountbonuses + "");
+            bonus = transfer / 100;
+        }
+        else  {
+            bonus = 0;
+        }
+
+        System.out.print("Accrued bonuses " + bonus + "");
         }
     }
